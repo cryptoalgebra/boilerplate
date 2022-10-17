2cf552593853fa7e0341a16de6a02db82232e3e9
IliaAzhel <aje-ilia2013@yandex.by>
2022-10-17 16:07:23 +0300

    init commit

---
+++ contracts/SimpleSwap.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity =0.7.6;
+pragma abicoder v2;
+
+import '@cryptoalgebra/periphery/contracts/interfaces/ISwapRouter.sol';
+import '@cryptoalgebra/periphery/contracts/libraries/TransferHelper.sol';
+
+contract SimpleSwap {
+   constructor() {
+   }
+}
