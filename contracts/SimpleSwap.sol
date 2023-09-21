07e55801752f86800f1e344980879a19801f9899
Ilya <31766003+IliaAzhel@users.noreply.github.com>
2023-09-21 06:45:40 +0300

    Update SimpleSwap

--- contracts/SimpleSwap.sol
+++ contracts/SimpleSwap.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity =0.7.6;
+pragma solidity =0.8.20;
 pragma abicoder v2;
 
-import '@cryptoalgebra/periphery/contracts/interfaces/ISwapRouter.sol';
-import '@cryptoalgebra/periphery/contracts/libraries/TransferHelper.sol';
+import '@cryptoalgebra/integral-periphery/contracts/interfaces/ISwapRouter.sol';
+import '@cryptoalgebra/integral-periphery/contracts/libraries/TransferHelper.sol';
 
 contract SimpleSwap {
    constructor() {
